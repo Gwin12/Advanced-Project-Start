10a73d488f021ac8236bbd8348e5f16bbdc8addf
gwin12 <nwfrgodwin@gmail.com>
2024-02-17 18:43:53 +0100

    initial update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err.stack);
     res.status(500).send("An error have occured!");
 });
